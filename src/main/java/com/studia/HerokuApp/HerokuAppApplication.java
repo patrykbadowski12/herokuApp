0be88a013f26085b6289c789c2cb7ac719a0150f
Patryk Badowski <patryk_badowski@o2.pl>
2020-04-25 18:24:28 +0100

    Init commit

---
+++ src/main/java/com/studia/HerokuApp/HerokuAppApplication.java
@@ -0,0 +1,13 @@
+package com.studia.HerokuApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HerokuAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HerokuAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/studia/HerokuApp/HerokuAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.studia.HerokuApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HerokuAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
